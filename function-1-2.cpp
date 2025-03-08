36c9f7a790f1e8d42814d635ddc9c44be9f2d5a3
Michael Murin <michael.murin@adelaide.edu.au>
2025-03-08 16:10:05 +1030

    Removed unnecessary files and fixed f-3-3 & f-1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -6,8 +6,8 @@ double array_mean(int array[], int n) {
     if (n < 1) {
         return 0.0;
     }
-    int sum = 0;
-    int mean = 0;
+    double sum = 0;
+    double mean = 0;
     for (int i = 0; i < n; i++) {
         sum = sum + array[i];
     }
--- function-3-3.cpp
+++ function-3-3.cpp
@@ -7,7 +7,7 @@ double weighted_average(int array[], int n) {
     if (n < 1) {
         return 0;
     }
-    int occur = 0;
+    double occur = 0;
     double weightAvg = 0;
     for (int i = 0; i < n; i++) {
         occur = 0;

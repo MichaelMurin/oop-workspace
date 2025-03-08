1fb6ad3817af159e8447af041bd4895d5e4ac1eb
Michael Murin <michael.murin@adelaide.edu.au>
2025-03-08 14:50:58 +1030

    H.Q.3

---
+++ function-3-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//function that given an array and its length, returns the following weighted average of the array,
+//defined as the sum of each element multiplied by its frequency divided by the total number of elements
+
+double weighted_average(int array[], int n) {
+    if (n < 1) {
+        return 0;
+    }
+    int occur = 0;
+    double weightAvg = 0;
+    for (int i = 0; i < n; i++) {
+        occur = 0;
+        for (int j = 0; j < n; j++) {
+            if (array[j] == array[i]) {
+                occur++;
+            }
+        }
+        weightAvg = weightAvg + (array[i] * occur / n);
+    }
+    return weightAvg;
+}

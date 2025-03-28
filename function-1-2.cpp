1a9824fd9306016b14aa3e0a422a5d51e8d87203
Michael Murin <michael.murin@adelaide.edu.au>
2025-03-28 11:49:32 +1030

    Jane Doe

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,7 +4,7 @@
 PersonList createPersonList(int n) {
     Person* array = new Person[n];
     for (int i = 0; i < n; i++) {
-        array[i] = (Person){"John Doe", 1};
+        array[i] = (Person){"Jane Doe", 1};
     }
     PersonList list = (PersonList){array, n};
     return list;

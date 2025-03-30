bef27c4eabd21ea796bd203fd1bfa716657374be
Michael Murin <michael.murin@adelaide.edu.au>
2025-03-31 09:18:42 +1030

    Musician code

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include "Person.h"
+#include "Musician.h"
 
 using namespace std;
 
-extern Person* createPersonArray(int n);
-
 int main() {
-    Person *arrayJohnDoe = createPersonArray(4);
-    for (int i = 0; i < 4; i++) {
-        cout << arrayJohnDoe[i].name << " " << arrayJohnDoe[i].age << endl;
-    }
-    delete[] arrayJohnDoe;
+    Musician muse1 = Musician();
+    Musician muse2 = Musician("Violin", 1);
+    Musician muse3 = Musician("Piano", 10);
+
+    cout << "I have played " << muse1.get_instrument() << " for " << muse1.get_experience() << " years." << endl;
+    cout << "I have played " << muse2.get_instrument() << " for " << muse2.get_experience() << " years." << endl;
+    cout << "I have played " << muse3.get_instrument() << " for " << muse3.get_experience() << " years." << endl;
     return 0;
 }

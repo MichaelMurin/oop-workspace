6ec1700e175f8bfccecc46baabd5bcf135d0a39d
Michael Murin <michael.murin@adelaide.edu.au>
2025-06-05 18:38:10 +0930

    exception handling experiment

--- ws9.cpp
+++ ws9.cpp
@@ -24,6 +24,12 @@ public:
         { 
             string line;
             while (getline(_file, line)) {
+                for (int i = 0; i < line.size(); i++) {
+                    if (isdigit(line[i])) {
+                        _file.close();
+                        throw std::out_of_range("character error");
+                    }
+                }
                 int value = stoi(line);
                 records.push_back(value);
             } 
@@ -36,17 +42,24 @@ int main(int argc, char* argv[]) {
     Records myRecords;
 
     string filename = "records.txt";
+
     if (argc > 1) filename = argv[1];
 
     RecordsManager recordM(filename); 
         
-    recordM.read(myRecords);
+    //recordM.read(myRecords);
+    try{
+        recordM.read(myRecords);
+    }catch(std::out_of_range const& e){
+        std::cerr << e.what() << "\n";
+    }
 
     int sum = 0;
     for (int i = 0; i < myRecords.size(); i++) {
         sum += myRecords[i];
     }
     cout << sum << endl;
+
 
 
     return 0;
